b2a0b52bfbbf77e6a8d23fc78c69a5e10cde75ad
Michal Szubarga <m.szubarga@hotmail.co.uk>
2020-07-16 10:42:17 +0000

    last for contact form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'snartscho@gmail.com'
+    default to: 'crusidlo@outlook.com'
     
     def contact_email(name, email, body)
         @name = name
